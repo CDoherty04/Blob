d4a9fd793a8603c651026c330d10bcdf2a4ae6aa
Charlie <charlieedoherty@gmail.com>
2024-03-19 12:34:08 -0500

    Remove try-except block to raise an error

--- file_processor.py
+++ file_processor.py
@@ -9,25 +9,20 @@ def get_map():
     the start position is not within range
     """
 
-    try:
+    # Have the user enter an input file
+    input_file = input("File name: ")
 
-        # Have the user enter an input file
-        input_file = input("File name: ")
+    with open(input_file, "r") as file:
+        lines = file.readlines()
 
-        with open(input_file, "r") as file:
-            lines = file.readlines()
+    lines = [line.strip() for line in lines]
 
-        lines = [line.strip() for line in lines]
+    # Check rows columns and start position
+    r, c = lines[0].split(" ")
+    sr, sc = lines[1].split(" ")
+    if int(r.strip()) < 1 or int(c.strip()) < 1 or int(sr.strip()) < 0 or int(sc.strip()) < 0:
+        raise IndexError("Rows and/or columns must be greater than 0")
+    if int(sr.strip())+1 > int(r.strip()) or int(sc.strip())+1 > int(c.strip()):
+        raise IndexError("Start position must be within the maps bounds")
 
-        # Check rows columns and start position
-        r, c = lines[0].split(" ")
-        sr, sc = lines[1].split(" ")
-        if int(r.strip()) < 1 or int(c.strip()) < 1 or int(sr.strip()) < 0 or int(sc.strip()) < 0:
-            raise IndexError("Rows and/or columns must be greater than 0")
-        if int(sr.strip())+1 > int(r.strip()) or int(sc.strip())+1 > int(c.strip()):
-            raise IndexError("Start position must be within the maps bounds")
-
-        return lines
-
-    except FileNotFoundError:
-        print("That file doesn't seem to exist.")
+    return lines
